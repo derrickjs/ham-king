388cd50faf4fe6e652106d2bdf73ef625aeed79e
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-11-26 17:58:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+const rURL = "https://raw.githubusercontent.com/derrickjs/ham-king/main/products.json"
+
+
+
+var request = new XMLHttpRequest();
+
+
+
+request.open("GET", rURL);
+request.responseType = "json";
+
+
+request.send();
+
+request.onload = () => {
+    classInfo(request.response);
+
+}
+
+
 var departments = ["Produce", "Bread", "Dairy", "Meat", "Seafood", "Drinks", "Deli", "Frozen"] ;
 
 
-var onSale = ["Dairy", "Bread", "Meat", "Drinks", "Frozen"];
+var onSale = ["Dairy" , "Bread" , "Meat", "Drinks", "Frozen" ];
+
+
+
+
+function classInfo(data) {
+    for( let d of data){
+        document.querySelector(".parent").innerHTML += "<section class = 'child'>" + "<h1>" + d.product + "</h1> " + "<p>" + d.description + "</p>"+ "<img class  = 'image' src = " + d.image+"  width = 80% >" + "<div class = 'prices' >" + "<h2 class ='price'>Normal price"+ d.normal_price + "</h2>" + "<h2 class = 'salePrice'> Sale price" +d.sale_price + "</h2>" + "</div" + "</section>";
+    }
+
+   
+}
+
+
+// I am unsure how to get this function below to hide the non sale ads. I used this from the video but and unsure how to use it with the function I wrote above. 
 
 const runAd = () => {
     for(var dept of departments ){
-        document.querySelector(".parent").innerHTML +=  "<section class =' child " + dept + " '><h1>" + dept + " </h1><p>price goes here</p></section>"
+       
 
         if(!onSale.includes(dept)){
             document.querySelector("." + dept).classList.add("hide");
