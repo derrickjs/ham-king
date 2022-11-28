a595b18dd67f6efcd1f382c75711948da8c512d6
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-11-28 09:53:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const rURL = "https://raw.githubusercontent.com/derrickjs/ham-king/main/products.json"
 
+var departments = ["Produce", "Bread", "Dairy", "Meat", "Seafood", "Drinks", "Deli", "Frozen"] ;
+var onSale = ["Dairy" , "Bread" , "Meat", "Drinks", "Frozen" ];
 
+
+
+
+
+
+
+
+const runAd = () => {
+    for(var dept of departments ){
+
+        document.querySelector(".parent").innerHTML += `<section class = '${dept}'> <h1>${dept}</h1></section>`
+        
+       
+
+        if(!onSale.includes(dept)){
+            document.querySelector("." + dept).classList.add("hide");
+        }
+    }
+}
+
+runAd();
 
 var request = new XMLHttpRequest();
-
 
 
 request.open("GET", rURL);
@@ -18,33 +40,10 @@ request.onload = () => {
 }
 
 
-var departments = ["Produce", "Bread", "Dairy", "Meat", "Seafood", "Drinks", "Deli", "Frozen"] ;
-
-
-var onSale = ["Dairy" , "Bread" , "Meat", "Drinks", "Frozen" ];
-
-
-
-
 function classInfo(data) {
     for( let d of data){
-        document.querySelector(".parent").innerHTML += "<section class = 'child'>" + "<h1>" + d.product + "</h1> " + "<p>" + d.description + "</p>"+ "<img class  = 'image' src = " + d.image+"  width = 80% >" + "<div class = 'prices' >" + "<h2 class ='price'>Normal price"+ d.normal_price + "</h2>" + "<h2 class = 'salePrice'> Sale price" +d.sale_price + "</h2>" + "</div" + "</section>";
+        document.querySelector(`.${d.department}`).innerHTML += "<section class = 'child'> " + "<section class = 'grandchild'>" + "<h1>" + d.product + "</h1> " + "<p>" + d.description + "</p>"+ "<img class  = 'image' src = " + d.image+"  width = 80% >" + "<div class = 'prices' >" + "<h2 class ='price'>Normal price"+ d.normal_price + "</h2>" + "<h2 class = 'salePrice'> Sale price" +d.sale_price + "</h2>" + "</div" + "</section>"+"</section>";
     }
 
    
 }
-
-
-// I am unsure how to get this function below to hide the non sale ads. I used this from the video but and unsure how to use it with the function I wrote above. 
-
-const runAd = () => {
-    for(var dept of departments ){
-       
-
-        if(!onSale.includes(dept)){
-            document.querySelector("." + dept).classList.add("hide");
-        }
-    }
-}
-
-runAd();
